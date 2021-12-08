a24d8fe67a91e9c96bf2e5bfc94dc2a522edd439
flint <olusegun.sola19@gmail.com>
2021-12-08 18:54:13 +0100

    added error component to fashion service

--- lib/Presentation/screen/services/add_fashion.dart
+++ lib/Presentation/screen/services/add_fashion.dart
@@ -321,7 +321,51 @@ class _AddServiceState extends State<AddService> {
                       ),
                     );
                   } else {
-                    return const Text("something went wrong");
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.error_outline,
+                            size: 40,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "An Error Occured",
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size.fromWidth(200),
+                                primary: primaryCol,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const SizedBox(
+                                width: 50,
+                                child: Text(
+                                  "Retry",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ))
+                        ],
+                      ),
+                    );
                   }
                 },
               ))
